d7a313464bbcd669078cee594b77e217dba27cb2
addieoun <98779645+addieoun@users.noreply.github.com>
2022-03-23 09:10:31 -0500

    Add files via upload

---
+++ customer/src/main/java/com/example/demo/customer/Customer.java
@@ -0,0 +1,81 @@
+package com.example.demo.customer;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    private String email;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String email, String password, String first_name, String last_name, String address) {
+        this.email = email;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+    }
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "first_name")
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    @Column(name = "last_name")
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
